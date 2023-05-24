9bdbed3624bd2596b826e4b534c7f8ee44ed8532
programmer <info2016frei@gmail.com>
2023-05-24 09:11:33 +0200

    case space O(1) if sort in place

--- easy/twoNumberSum/twoNumberSum.py
+++ easy/twoNumberSum/twoNumberSum.py
@@ -9,7 +9,9 @@ def twoNumberSumNaive(array, targetSum):
     param: targetSum: int
     return: list of two numbers that sum up to targetSum
     """
-    
+    # edge case if array is empty
+    if len(array) == 0:
+        return []
     for i in range(len(array)-1):
         firstNum = array[i]
         for j in range(i+1, len(array)):
@@ -22,12 +24,15 @@ def twoNumberSumNaive(array, targetSum):
 # time O(n) space O(n)
 def twoNumberSumDict(array, targetSum):
     """ use the fact that targetSum = firstNum + secondNum 
-    so secondNum = targetSum - firstNum
-    use a dictionary to store the secondNum and check if it is in the dictionary
-    param: array: list of numbers
-    param: targetSum: int
-    return: list of two numbers that sum up to targetSum
-    """
+        so secondNum = targetSum - firstNum
+        use a dictionary to store the secondNum and check if it is in the dictionary
+        param: array: list of numbers
+        param: targetSum: int
+        return: list of two numbers that sum up to targetSum
+        """
+    # edge case if array is empty
+    if len(array) == 0:
+        return []
     nums = {}
     for num in array:
         potentialMatch = targetSum - num
@@ -35,4 +40,31 @@ def twoNumberSumDict(array, targetSum):
             return [potentialMatch, num]
         else:
             nums[num] = True
+    return []
+
+# time O(nlog(n)) space O(1)
+def twoNumberSumSort(array, targetSum):
+    """ sort the array first value of
+    left pointer and right pointer and compare with targetSum to move the pointers
+    if numSum > targetSum move right pointer to the left else move left pointer to the right
+    until left pointer is greater than right pointer then return empty list 
+    
+        param: array: list of numbers
+        param: targetSum: int
+        return: list of two numbers that sum up to targetSum
+    """
+    # edge case if array is empty
+    if len(array) == 0:
+        return []
+    lelft_pointer = 0
+    right_pointer = len(array) - 1
+    array.sort()
+    while lelft_pointer < right_pointer:
+        numSum = array[lelft_pointer] + array[right_pointer]
+        if numSum == targetSum:
+            return [array[lelft_pointer], array[right_pointer]]
+        elif numSum > targetSum:
+            right_pointer -= 1
+        elif numSum < targetSum:
+            lelft_pointer += 1
     return []
