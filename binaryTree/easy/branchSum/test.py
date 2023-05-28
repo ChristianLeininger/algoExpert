200690ff619708968c84e7dd9dcd4218a8f6b137
programmer <info2016frei@gmail.com>
2023-05-28 22:42:58 +0200

    add test

---
+++ binaryTree/easy/branchSum/test.py
@@ -0,0 +1,14 @@
+# copyright 2023
+# author: Christian Leininger
+# Email: leininic @ tf.uni-freiburg.de
+# date: 28.05.2023
+
+
+from brunchSum import BinaryTree, branchSums
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        tree = BinaryTree(1).insert([2, 3, 4, 5, 6, 7, 8, 9, 10])
+        self.assertEqual(branchSums(tree), [15, 16, 18, 10, 11])
