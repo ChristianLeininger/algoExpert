e0ddefd0fd95f114ff045d28fa9505b409bba4c7
programmer <info2016frei@gmail.com>
2023-05-31 07:06:53 +0200

    add test

---
+++ binarySearchTree/easy/findClosestValueInBst/test.py
@@ -0,0 +1,46 @@
+# copyright 2023
+# author: Christian Leininger
+# Email: leininic @ tf.uni-freiburg.d
+# date: 25.05.2023
+
+import unittest
+from findClosestValueInBst import findClosestValueInBstIterative
+from findClosestValueInBst import findClosestValueInBstRecursive
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.left.left = BST(2)
+        root.left.left.left = BST(1)
+        root.left.right = BST(5)
+        root.right = BST(15)
+        root.right.left = BST(13)
+        root.right.left.right = BST(14)
+        root.right.right = BST(22)
+        expected = 13
+        actual = findClosestValueInBstIterative(root, 12)
+        self.assertEqual(expected, actual)
+
+    def test_case_2(self):
+        root = BST(10)
+        root.left = BST(5)
+        root.left.left = BST(2)
+        root.left.left.left = BST(1)
+        root.left.right = BST(5)
+        root.right = BST(15)
+        root.right.left = BST(13)
+        root.right.left.right = BST(14)
+        root.right.right = BST(22)
+        expected = 13
+        actual = findClosestValueInBstRecursive(root, 12)
+        self.assertEqual(expected, actual)
+
