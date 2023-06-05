c3b06d239ea6610a6cdaa51e8fc21a28808cf285
programmer <info2016frei@gmail.com>
2023-06-05 07:06:40 +0200

    add to test naive sol

--- array/medium/arrayOfProducts/test.py
+++ array/medium/arrayOfProducts/test.py
@@ -3,12 +3,18 @@
 # Email: leininic@tf.uni-freiburg.de
 # date: 01.06.2023
 
-from arrayOfProducts import arrayOfProducts
+from arrayOfProducts import arrayOfProducts, arrayOfProductsNaive
 import unittest
 
 
 class TestProgram(unittest.TestCase):
     def test_case_1(self):
+        array = [5, 1, 4, 2]
+        expected = [8, 40, 10, 20]
+        actual = arrayOfProducts(array)
+        self.assertEqual(actual, expected)
+
+    def test_case_2(self):
         array = [5, 1, 4, 2]
         expected = [8, 40, 10, 20]
         actual = arrayOfProducts(array)
