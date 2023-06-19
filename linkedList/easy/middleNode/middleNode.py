d91bef315c17ef14ad890bdbdd7fa7641a5722d4
programmer <info2016frei@gmail.com>
2023-06-19 09:59:41 +0200

    add naive middle node

---
+++ linkedList/easy/middleNode/middleNode.py
@@ -0,0 +1,39 @@
+# copyright 2023
+# author: Christian Leininger
+# Email: leininic@tf.uni-freiburg.de
+# date: 18.06.2023
+
+
+class LinkedList:
+    def __init__(self, value: int):
+        self.value = value
+        self.next = None
+
+
+# O(n) time | O(1) space
+def middleNodeNaive(linkedList: LinkedList) -> LinkedList:
+    """ Return the middle node of the linked list
+        naive approach with two passes
+        first pass count the length of the linked list
+        second pass return the middle node
+    Args:
+        param1: linkedList: LinkedList
+
+    Return: LinkedList
+    """
+    length = 0
+    element = linkedList
+    while element is not None:
+        length += 1
+        element = element.next
+    middle = length // 2
+    element = linkedList
+    count = 0
+    while count < middle:
+        count += 1
+        element = element.next
+    return element
+
+
+def middleNode(linkedList: LinkedList) -> LinkedList:
+    pass
