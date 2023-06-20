a5bb3bd9895cdf7ec31d27d2e6e192f015156038
programmer <info2016frei@gmail.com>
2023-06-20 22:58:39 +0200

    add improved version

--- strings/medium/longestPalindromicSubstring/longestPalindromicSubstring.py
+++ strings/medium/longestPalindromicSubstring/longestPalindromicSubstring.py
@@ -59,7 +59,57 @@ def generate_substrings(s: str) -> List[str]:
 # O(n^2) time | O(n) space
 def longestPalindromicSubstring(string: str) -> str:
     """ Find the longest palindromic substring in the input string """
-    return
+
+    current_range = [0, 1]
+    for i in range(len(string)):
+        odd = check_odd(string, i)
+        even = check_even(string, i)
+        if odd > even:
+
+            new_range = [i - odd // 2, i + odd // 2 + 1]
+            # import pdb; pdb.set_trace()
+        else:
+            new_range = [i - even // 2, i + even // 2]
+            # print(f"solution {string[new_range[0]:new_range[1]]}" )
+        if new_range[1] - new_range[0] > current_range[1] - current_range[0]:
+            current_range = new_range
+    return string[current_range[0]:current_range[1]]
+
+
+def check_odd(string: str, idx: int) -> int:
+    """ compute the longest palindrom with odd length
+    Args:
+        param1(str): input string
+        param2(int): index of the string
+    Return:
+        int: length of the palindrom at idx
+    """
+    left_idx = idx - 1
+    right_idx = idx + 1
+    while left_idx >= 0 and right_idx < len(string):
+        if string[left_idx] != string[right_idx]:
+            break
+        left_idx -= 1
+        right_idx += 1
+    return right_idx - left_idx - 1
+
+
+def check_even(string: str, idx: int) -> int:
+    """  check at position idx if the string is a palindrom with even length
+
+    Args:
+        param1(str): input string
+        param2(int): index of the string
+    Return: length of the palindrom at idx
+    """
+    left_idx = idx - 1
+    right_idx = idx
+    while left_idx >= 0 and right_idx < len(string):
+        if string[left_idx] != string[right_idx]:
+            break
+        left_idx -= 1
+        right_idx += 1
+    return right_idx - left_idx - 1
 
 
 if __name__ == '__main__':
