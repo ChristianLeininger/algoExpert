28e19c6ead1de1d9f88ba528b0160eefbbb5ffad
programmer <info2016frei@gmail.com>
2023-06-20 22:59:34 +0200

    add various test longest palindrom

--- strings/medium/longestPalindromicSubstring/test.py
+++ strings/medium/longestPalindromicSubstring/test.py
@@ -6,7 +6,9 @@
 
 import unittest
 from longestPalindromicSubstring import longestPalindromicSubstringNaive
+from longestPalindromicSubstring import longestPalindromicSubstring
 from longestPalindromicSubstring import is_palindrom, generate_substrings
+from longestPalindromicSubstring import check_odd, check_even
 
 
 class TestProgram(unittest.TestCase):
@@ -26,3 +28,31 @@ class TestProgram(unittest.TestCase):
     def test_case_5(self):
         self.assertEqual(
                 longestPalindromicSubstringNaive("abaxyzzyxf"), "xyzzyx")
+
+    def test_case_6(self):
+        self.assertEqual(
+                check_odd(string="aba", idx=1), 3)
+
+    def test_case_7(self):
+        self.assertEqual(
+                check_odd(string="dcabacd", idx=1), 1)
+
+    def test_case_8(self):
+        self.assertEqual(
+                check_odd(string="dcabacd", idx=3), 7)
+
+    def test_case_9(self):
+        self.assertEqual(
+                check_even(string="dcabbacd", idx=4), 8)
+
+    def test_case_10(self):
+        self.assertEqual(
+                longestPalindromicSubstring("abaxyzzyxf"), "xyzzyx")
+
+    def test_case_11(self):
+        self.assertEqual(
+                longestPalindromicSubstring("abcdefghfedcbaa"), "aa")
+
+    def test_case_12(self):
+        self.assertEqual(
+                longestPalindromicSubstring("ab12365456321bb"), "b12365456321b")
